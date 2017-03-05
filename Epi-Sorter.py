4d2dd0c83fc4d8e30dd66705c12310126ddb3dc0
divastor <divastor@users.noreply.github.com>
2017-03-05 16:23:06 +0200

    Add files via upload

--- Epi-Sorter.py
+++ Epi-Sorter.py
@@ -21,11 +21,29 @@ while j <= n:
     
 filenames = os.listdir(os.getcwd())
 # - Get every file in directory
-
+print filenames
 for fn in filenames:
 # - Check every file serially
     episode = re.findall(r'E\d{1,2}', fn, re.I)
     season = re.findall(r'S\d{1,2}', fn, re.I) #not that needful stuff
+    if fn.lower() == "subtitles" or fn.lower() == "subs" or fn.lower() == "subtitle" or fn.lower() == "sub":
+        fn_filenames = os.listdir(os.getcwd()+"\\"+fn)
+        print fn_filenames
+        for fnsfn in fn_filenames:
+            try:
+                episode = re.findall(r'E\d{1,2}', fnsfn, re.I)
+                j = int(episode[0][1:])
+                if j < 10:
+                    name = 'EPISODE 0' + str(j)
+                else:
+                    name = 'EPISODE ' + str(j)
+
+                os.rename(fn+"\\"+fnsfn, name+'\\'+fnsfn)
+            except:
+                pass
+
+        continue
+    
     try:
         j = int(episode[0][1:])
         if j < 10:
